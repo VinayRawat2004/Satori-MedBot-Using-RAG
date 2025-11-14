49f74cb5479584da3589f84f53abe60549380eca
VinayRawat2004 <152405338+VinayRawat2004@users.noreply.github.com>
2025-11-14 15:15:27 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -17,7 +17,7 @@ app = FastAPI(title="MedBot RAG API")
 # CORS for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://medbot-ui.vercel.app"]
+    allow_origins=["http://localhost:3000", "https://medbot-ui.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -66,4 +66,5 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
